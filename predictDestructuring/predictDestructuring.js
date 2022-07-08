2a38e01d0b24ec7418075223f5148d90d075490c
joshuabecker91 <joshua.becker91@gmail.com>
2022-07-07 23:53:10 -0700

    uploading predictDestructuring assignment

---
+++ predictDestructuring/predictDestructuring.js
@@ -0,0 +1,77 @@
+// For the following five blocks of code, predict the output. If a line of code will throw an error, state the error.
+
+// 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// Output: "Tesla"
+// Output: "Mercedes"
+
+
+// 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+// Output: N/A
+// Output: error, invalid reference name not defined
+
+
+// 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+// Output: 12345
+// Output: error - need to pass something into the function, not defined
+
+
+// 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+// first = 2, second = 5, third = 2
+// Output: false
+// Output: true
+
+
+// 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// Output: value
+// Output: [1, 5, 1, 8, 3, 3]
+// Output: 1
+// Output: 5
