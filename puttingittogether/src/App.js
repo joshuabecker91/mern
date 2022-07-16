a4cace0a6b67c697c2b3a5e2b4881222af6f8e4b
joshuabecker91 <joshua.becker91@gmail.com>
2022-07-16 12:36:35 -0700

    updating

--- puttingittogether/src/App.js
+++ puttingittogether/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <PersonCard firstName={"Sam"} lastName={"Smith"} age={67} hairColor={"brown"} />
       <PersonCard firstName={"Will"} lastName={"Smith"} age={37} hairColor={"black"} />
       <PersonCard firstName={"Bill"} lastName={"Smith"} age={47} hairColor={"blonde"} />
-      <PersonCard firstName={"Rob"} lastName={"Justice"} age={27} hairColor={"pink"} />
+      <PersonCard firstName={"Rob"} lastName={"Justice"} age={27} hairColor={"black"} />
     </div>
   );
 }
